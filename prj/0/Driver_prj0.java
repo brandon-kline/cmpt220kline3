025d328ca196627f4c30e17a73c6769893755934
brandon-kline <Brandon.Kline1@marist.edu>
2017-01-18 21:26:37 -0500

    Longer msg.

---
+++ prj/0/Driver_prj0.java
@@ -0,0 +1,26 @@
+    /**
+   * file: Driver_prj0.java
+   * author: Brandon Kline
+   * course: CMPT 220
+   * assignment: project 0
+   * due date: Jaunuary 24, 2017
+   * version: 1.0
+   * 
+   * This file contains the declaration of the 
+   * SinglyLinkedList abstract data type.
+   */
+   
+    /**
+   * Driver_prj0
+   * 
+   * This class allows for the text
+   * "Hello World!" to be printed when the
+   * program is eun.
+   */
+
+  
+  public class Driver_prj0{
+  public static void main(String[] args) {
+	  System.out.println("Hello World!");
+	  }
+	  }
